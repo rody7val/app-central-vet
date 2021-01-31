5bfdd8b2ab3a36d944da1acb8191858c5255f88a
Rodolfo Valguarnera <rod7val@gmail.com>
2021-01-31 20:24:39 -0300

    init

---
+++ App.vue
@@ -0,0 +1,37 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">{{ message }}</text>
+    <button title="Press me!" @press="exclaim" />
+    <button :title="message" @press="handleBtnPress"/>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World"
+    };
+  },
+  methods: {
+    exclaim() {
+      this.message += "!";
+    },
+    handleBtnPress() {
+      alert(this.message);
+    }
+  },
+};
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
